4a6b0247299c83a80be6478ae66ef15e967e0f60
agreco <greco031@umn.edu>
2023-03-11 14:11:39 -0600

    Data Cleaning complete. Gender and Condition columns not capitalized

--- R/week7.R
+++ R/week7.R
@@ -1,9 +1,22 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
-
+library(lubridate)
 
 
 
 # Data Import and Cleaning
-week7_tbl <- read_csv(file ="../data/week3.csv")
+week7_tbl <- read_csv(file ="../data/week3.csv") %>%
+  mutate(condition = case_match(condition, "A" ~ "Block A", "B" ~ "Block B", "C" ~ "Control")) %>%
+  mutate(gender = case_match(gender, "M" ~ "Male", "F" ~ "Female")) %>% 
+  #rename(Condition = condition) %>%
+  #rename(Gender = gender) %>%
+  filter(q6 == 1) %>%
+  select(!q6) %>%
+  mutate(timeStart = ymd_hms(timeStart)) %>%
+  mutate(timeSpent = difftime(timeEnd, timeStart, units = c("mins")))
+
+
+
+
+
