f8b8dba13da14b461162f7b7bae424ef73194dbc
agreco <greco031@umn.edu>
2023-03-11 17:59:06 -0600

    Added cartesian coordinates for images 2, 3 and 5. Still works!

--- R/week7.R
+++ R/week7.R
@@ -30,13 +30,14 @@ GGally::ggpairs(week7_tbl[5:13])
 
 (ggplot(week7_tbl, aes(x = q1, y = q2, color = gender)) +
   geom_point(position = "jitter") +
-  labs(color = "Participant Gender")) %>%
+  labs(color = "Participant Gender") + 
+  coord_fixed()) %>%
   ggsave(filename = "../figs/fig2.png", dpi = 600, width = 7, height = 5, units = "in")
-
 (ggplot(week7_tbl, aes(x = q1, y = q2)) +
   geom_point(position = "jitter") +
-  labs(x = "Score on Q1", y = "Score on Q2") +
-  facet_grid(cols = vars(gender))) %>%
+  facet_grid(cols = vars(gender))  + 
+  coord_fixed() +
+  labs(x = "Score on Q1", y = "Score on Q2")) %>%
   ggsave(filename = "../figs/fig3.png", dpi = 600, width = 7, height = 5, units = "in")
 
 
@@ -48,7 +49,8 @@ GGally::ggpairs(week7_tbl[5:13])
 
 (ggplot(week7_tbl, aes(x = q5, y = q7, color = condition)) + 
   geom_point(position = "jitter") +
-  geom_smooth(method = lm, se = FALSE) +
+  geom_smooth(method = lm, se = FALSE) + 
+  coord_fixed(ratio = 0.5) +
   labs(x = "Score on Q5", y = "Score on Q7", color = "Experimental Condition") +
   theme(legend.background = element_rect(fill = "#E0E0E0"), legend.position = "bottom")) %>% 
   ggsave(filename = "../figs/fig5.png", dpi = 600, width = 7, height = 5, units = "in")
