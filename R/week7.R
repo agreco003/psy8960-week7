8843e75297dcd0e8c589b62d77e7f37724cd1b69
agreco <greco031@umn.edu>
2023-03-11 15:58:53 -0600

    Figs 1 - 3 complete, fig 4 working but not saved. Recast condition and gender as factors, timeSpent as numeric. Deleted rename rows comments

--- R/week7.R
+++ R/week7.R
@@ -2,21 +2,44 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(lubridate)
+library(GGally)
 
 
 
 # Data Import and Cleaning
 week7_tbl <- read_csv(file ="../data/week3.csv") %>%
-  mutate(condition = case_match(condition, "A" ~ "Block A", "B" ~ "Block B", "C" ~ "Control")) %>%
-  mutate(gender = case_match(gender, "M" ~ "Male", "F" ~ "Female")) %>% 
-  #rename(Condition = condition) %>%
-  #rename(Gender = gender) %>%
+  mutate(condition = factor(condition, levels = c("A", "B", "C"), labels = c("Block A", "Block B", "Control"))) %>%
+  mutate(gender = factor(gender, levels = c("M", "F"), labels = c("Male", "Female"))) %>% 
   filter(q6 == 1) %>%
   select(!q6) %>%
   mutate(timeStart = ymd_hms(timeStart)) %>%
-  mutate(timeSpent = difftime(timeEnd, timeStart, units = c("mins")))
+  mutate(timeSpent = as.numeric(difftime(timeEnd, timeStart, units = c("mins"))))
 
 
 
 
+# Visualization
+ggpairs(week7_tbl[5:13]) #Scatterplots lower left, cor top right, density along diag = default
 
+
+(ggplot(week7_tbl, aes(x = timeStart, y = q1)) +
+  geom_point() +
+  labs(x = "Date of Experiment", y = "Q1 Score")) %>%
+  ggsave(filename = "../figs/fig1.png", dpi = 600, width = 7, height = 5, units = "in") 
+
+
+(ggplot(week7_tbl, aes(x = q1, y = q2, color = gender)) +
+  geom_point(position = "jitter") +
+  labs(color = "Participant Gender")) %>%
+  ggsave(filename = "../figs/fig2.png", dpi = 600, width = 7, height = 5, units = "in")
+
+(ggplot(week7_tbl, aes(x = q1, y = q2)) +
+  geom_point(position = "jitter") +
+  labs(x = "Score on Q1", y = "Score on Q2") +
+  facet_grid(cols = vars(gender))) %>%
+  ggsave(filename = "../figs/fig3.png", dpi = 600, width = 7, height = 5, units = "in")
+
+
+ggplot(week7_tbl, aes(x = gender, y = timeSpent)) +
+  geom_boxplot() + 
+  labs(x = "Gender", y = "Time Elapsed (mins)")
